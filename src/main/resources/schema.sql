d3c81eab4a97a81c73eb095c625ebf171c956f43
KIMSIYOUNG <tldud2404@gmail.com>
2020-07-13 17:24:40 +0900

    feat : 스키마 구현

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,4 +13,69 @@ create table if not exists ACCOUNT
     STATE      varchar(16)  not null,
     EMAIL      binary,
     CREATED_AT datetime     not null
-)
+);
+
+create table if not exists REPO
+(
+    ID          varchar(255) not null primary key,
+    NAME        varchar(128) not null,
+    DESCRIPTION varchar(255),
+    CREATED_BY  bigint       not null,
+    CREATED_AT  datetime     not null
+);
+
+create table if not exists ISSUE
+(
+    ID         bigint      not null primary key auto_increment,
+    VERSION    integer,
+    REPO_ID    bigint      not null,
+    ISSUE_NO   bigint      not null,
+    STATUS     varchar(64) not null,
+    CREATED_BY varchar(36) not null,
+    CREATED_AT datetime    not null
+);
+
+create table if not exists ISSUE_CONTENT
+(
+    ID          bigint auto_increment primary key not null,
+    LABEL_ID    varchar(36)                       not null,
+    ATTACHED_AT datetime                          not null,
+    ISSUE_ID    varchar(36)                       not null
+);
+
+create table if not exists ISSUE_ATTACHED_LABEL
+(
+    ID          bigint auto_increment primary key not null,
+    LABEL_ID    bigint                            not null,
+    ATTACHED_AT datetime                          not null,
+    ISSUE_ID    bigint                            not null
+);
+
+create table if not exists LABEL
+(
+    ID      bigint auto_increment primary key not null,
+    REPO_ID bigint                            not null,
+    NAME    varchar(128)                      not null,
+    COLOR   varchar(128)                      not null
+);
+
+create table if not exists COMMENT
+(
+    ID bigint auto_increment primary key not null,
+    VERSION bigint,
+    ISSUE_ID bigint not null ,
+    CREATED_BY bigint not null ,
+    CREATED_AT datetime not null
+);
+
+create table if not exists COMMENT_TYPE
+(
+    ID bigint primary key ,
+    BODY varchar(255) not null ,
+    MIME_TYPE varchar(32) not null
+);
+
+
+
+
+
